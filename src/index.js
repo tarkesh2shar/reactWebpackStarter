2994e9f03a5445af94918add8a3dd7ed1248283a
unknown <tarkesh2shar@gmail.com>
2019-08-10 11:44:26 +0530

    first Commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './index.css';
+import BigImage from '../assets/1.jpg';
+import SmallImage from '../assets/3.jpg';
+
+const App=()=>{
+
+    // setTimeout(()=>{
+    //     System.import('../assets/1.jpg')
+    // },5000)
+
+    console.log("Running this component!")
+    return(
+        <h1>Hello World!</h1>
+    )
+}
+
+const as=(ReactComponent)=>{
+    console.log("Hello Worlddddddddddd!");
+    //alert("d!dDsSDAAAAAAAAAAAAAAAa")
+
+    fetch('server/products')
+  .then(response => response.json())
+  .then(json => console.log(json))
+
+    consolde.log(ReactComponent);
+
+    
+}
+
+
+as(App);
+
+
+
+
+
