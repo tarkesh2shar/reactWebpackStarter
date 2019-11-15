7e340ec81cd602cb09197c830bc694c9bb25c5fa
tarkesh2shar <tarkesh2shar@gmail.com>
2019-11-15 13:45:09 +0530

    Fixed final build error

--- dist/styles.7aa27f89d250d0ed593a.js
+++
@@ -1,3 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{4:function(n,w,o){}}]);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzdHlsZXMuN2FhMjdmODlkMjUwZDBlZDU5M2EuanMiLCJzb3VyY2VSb290IjoiIn0=
-//# sourceMappingURL=styles.7aa27f89d250d0ed593a.js.map
--- src/index.js
+++ src/index.js
@@ -1,37 +1,26 @@
-import React from 'react';
-import './index.css';
-import BigImage from '../assets/1.jpg';
-import SmallImage from '../assets/3.jpg';
+import React from "react";
+import "./index.css";
+import BigImage from "../assets/1.jpg";
+import SmallImage from "../assets/3.jpg";
 
-const App=()=>{
+const App = () => {
+  // setTimeout(()=>{
+  //     System.import('../assets/1.jpg')
+  // },5000)
 
-    // setTimeout(()=>{
-    //     System.import('../assets/1.jpg')
-    // },5000)
+  console.log("Running this component!");
+  return <h1>Hello World!</h1>;
+};
 
-    console.log("Running this component!")
-    return(
-        <h1>Hello World!</h1>
-    )
-}
+const as = ReactComponent => {
+  console.log("Hello Worlddddddddddd!");
+  alert("Updated Code here for debugging");
 
-const as=(ReactComponent)=>{
-    console.log("Hello Worlddddddddddd!");
-    //alert("d!dDsSDAAAAAAAAAAAAAAAa")
+  fetch("server/products")
+    .then(response => response.json())
+    .then(json => console.log(json));
 
-    fetch('server/products')
-  .then(response => response.json())
-  .then(json => console.log(json))
-
-    consolde.log(ReactComponent);
-
-    
-}
-
+  consolde.log(ReactComponent);
+};
 
 as(App);
-
-
-
-
-
