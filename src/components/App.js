ee6fab0ba14a37f88b722acaf4750fe249b24852
tarkesh2shar <tarkesh2shar@gmail.com>
2020-03-08 11:46:36 +0530

    added js hot reloading and few react boilerPlate

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import '../index.scss';
+import "../index.css";
+export default function App() {
+    return (
+        <div>
+            This is React
+        </div>
+    )
+}
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,31 +1,18 @@
-import React from "react";
-import "./index.scss";
-import "./index.css";
+import React from 'react';
+import ReactDom from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import App from './components/App';
+
+//extras just to test//
 import BigImage from "../assets/1.jpg";
 import SmallImage from "../assets/3.jpg";
-if(module && module.hot) module.hot.accept()
-const App = () => {
-  // setTimeout(()=>{
-  //     System.import('../assets/1.jpg')
-  // },5000)
 
-  console.log("Running this component!");
-  return <h1>Hello World!</h1>;
-};
-
-const as = ReactComponent => {
-  console.log("Hello Worlddddddddddd!");
- // alert("Updated Code here for debugging");
- alert(process.env.NODE_ENV)
- document.querySelector("body").innerHTML="Hello S WDODRLFDDD"
-
-  fetch("server/products")
-    .then(response => response.json())
-    .then(json => console.log(json));
-
-  consolde.log(ReactComponent);
-};
-
-
-
-as(App);
+ReactDom.render(<App />,
+    document.querySelector("#root")
+);
+if (module.hot) {
+    module.hot.accept(App, function () {
+        console.log('Accepting the updated printMe module!');
+        printMe();
+    })
+}
