e15f77581a7589bcd996ea2d605d5df9a6af3019
tarkesh2shar <tarkesh2shar@gmail.com>
2020-04-04 23:09:08 +0530

    adding css and sass prefixes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,7 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import '../index.scss';
-import "../index.css";
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import '../index.scss'
+import '../index.css'
 export default function App() {
-    return (
-        <div className="container mt-2">
-            This is React
-        </div>
-    )
+	return <div className='container mt-2'>This is React</div>
 }
-
-
-
--- webpack.common.js
+++ webpack.common.js
@@ -1,142 +1,155 @@
-const path = require("path");
-const miniCssExtract = require("mini-css-extract-plugin");
-const htmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
+const path = require('path')
+const miniCssExtract = require('mini-css-extract-plugin')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const autoprefixer = require('autoprefixer')
 
-const ASSET_PATH = process.env.ASSET_PATH || "/";
+const ASSET_PATH = process.env.ASSET_PATH || '/'
 
 module.exports = {
-  entry: ["babel-polyfill", "./src/index.js"],
+	entry: ['babel-polyfill', './src/index.js'],
 
-  // entry:{
+	// entry:{
 
-  //   bundle:"./src/index.js",
-  //   vendor:VENDOR_LIBS
-  // },
+	//   bundle:"./src/index.js",
+	//   vendor:VENDOR_LIBS
+	// },
 
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[hash].js",
-    publicPath: ""
-  },
-  module: {
-    //Specify all loaders inside rules array!!
-    rules: [
-      {
-        use: "babel-loader",
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/
-      },
-      // {
-      //     //The following Command inserts CSS directly inside javaScript as some form of text
-      //     use:["style-loader","css-loader"],
-      //     test:/\.css$/,
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].[hash].js',
+		publicPath: '',
+	},
+	module: {
+		//Specify all loaders inside rules array!!
+		rules: [
+			{
+				use: 'babel-loader',
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+			},
+			// {
+			//     //The following Command inserts CSS directly inside javaScript as some form of text
+			//     use:["style-loader","css-loader"],
+			//     test:/\.css$/,
 
-      // },
+			// },
 
-      {
-        test: /\.(jpe?g|png|gif|svg)$/,
-        use: [
-          {
-            loader: "url-loader",
-            options: { limit: 40000 }
-          },
-          "image-webpack-loader"
-        ]
-      },
+			{
+				test: /\.(jpe?g|png|gif|svg)$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: { limit: 40000 },
+					},
+					'image-webpack-loader',
+				],
+			},
 
-      {
-        test: /\.css$/,
-        //we use extract-text-plugin to get hold of that css and insert it into some sort of a file!
-        use: [
-          {
-            loader:"css-hot-loader"
-          },
-          {
-            loader: miniCssExtract.loader,
-            // options: {
-            //   // only enable hot in development
-            //   hmr: process.env.NODE_ENV === "development",
-            //   // if hmr does not work, this is a forceful method.
-            //   reloadAll: true
-            // }
-          },
-          {
-            loader: "css-loader"
-          }
-        ]
-      },
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				//we use extract-text-plugin to get hold of that css and insert it into some sort of a file!
+				use: [
+					{
+						loader: 'css-hot-loader',
+					},
+					{
+						loader: miniCssExtract.loader,
+						// options: {
+						//   // only enable hot in development
+						//   hmr: process.env.NODE_ENV === "development",
+						//   // if hmr does not work, this is a forceful method.
+						//   reloadAll: true
+						// }
+					},
+					{
+						loader: 'css-loader',
+					},
+					{
+						loader: 'postcss-loader',
+					},
+				],
+			},
 
-      {
-        test: /\.s[ac]ss$/i,
-        //we use extract-text-plugin to get hold of that css and insert it into some sort of a file!
-        use: [
-          {
-            loader:"css-hot-loader"
-          },
-          {
-            loader: miniCssExtract.loader,
-            // options: {
-            //   // only enable hot in development
-            //   hmr: process.env.NODE_ENV === "development",
-            //   // if hmr does not work, this is a forceful method.
-            //   reloadAll: true
-            // }
-          },
-          {
-            loader: "css-loader"
-          },
-          {
-            loader: "sass-loader"
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new miniCssExtract({
-      // Options similar to the same options in webpackOptions.output
-      // all options are optional
-      filename: "[name].css"
-      // chunkFilename: '[id].css',
-      // ignoreOrder: false, // Enable to remove warnings about conflicting order
-    }),
-    new htmlWebpackPlugin({
-      template: "src/index.html"
-    }),
-    new webpack.DefinePlugin({
-      "process.env.ASSET_PATH": JSON.stringify(ASSET_PATH)
-    }),
-    new webpack.SourceMapDevToolPlugin({})
-  ],
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        styles: {
-          name: "styles",
-          test: /\.css$/,
-          chunks: "all",
-          enforce: true
-        },
+			{
+				test: /\.s[ac]ss$/i,
+				exclude: /node_modules/,
+				//we use extract-text-plugin to get hold of that css and insert it into some sort of a file!
+				use: [
+					{
+						loader: 'css-hot-loader',
+					},
+					{
+						loader: miniCssExtract.loader,
+						// options: {
+						//   // only enable hot in development
+						//   hmr: process.env.NODE_ENV === "development",
+						//   // if hmr does not work, this is a forceful method.
+						//   reloadAll: true
+						// }
+					},
+					{
+						loader: 'css-loader',
+					},
+					{
+						loader: 'postcss-loader',
+					},
+					{
+						loader: 'sass-loader',
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new miniCssExtract({
+			// Options similar to the same options in webpackOptions.output
+			// all options are optional
+			filename: '[name].css',
+			// chunkFilename: '[id].css',
+			// ignoreOrder: false, // Enable to remove warnings about conflicting order
+		}),
+		new htmlWebpackPlugin({
+			template: 'src/index.html',
+		}),
+		new webpack.DefinePlugin({
+			'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH),
+		}),
+		new webpack.SourceMapDevToolPlugin({}),
+		new webpack.LoaderOptionsPlugin({
+			options: {
+				postcss: [autoprefixer()],
+			},
+		}),
+	],
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				styles: {
+					name: 'styles',
+					test: /\.css$/,
+					chunks: 'all',
+					enforce: true,
+				},
 
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module) {
-            // get the name. E.g. node_modules/packageName/not/this/part.js
-            // or node_modules/packageName
-            const packageName = module.context.match(
-              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-            )[1];
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name(module) {
+						// get the name. E.g. node_modules/packageName/not/this/part.js
+						// or node_modules/packageName
+						const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
 
-            // npm package names are URL-safe, but some servers don't like @ symbols
-            return `npm.${packageName.replace("@", "")}`;
-          },
-          chunks: "all",
-          maxInitialRequests: Infinity,
-          minSize: 0
-        }
-      }
-    }
-  }
-};
+						// npm package names are URL-safe, but some servers don't like @ symbols
+						return `npm.${packageName.replace('@', '')}`
+					},
+					chunks: 'all',
+					maxInitialRequests: Infinity,
+					minSize: 0,
+				},
+			},
+		},
+	},
+}
