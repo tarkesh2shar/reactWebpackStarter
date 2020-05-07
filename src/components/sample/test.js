4e5470dbdc54d2ffc82ba31aed27f2d4a7db71de
tarkesh2shar <tarkesh2shar@gmail.com>
2020-05-08 03:55:24 +0530

    added Readme file

--- src/components/sample/test.js
+++ src/components/sample/test.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import './index.scss'
+import Image from '../../../assets/1.jpg'
 
 export default function Test() {
-	return <div className='test__flex'>Test Here</div>
+	return (
+		<React.Fragment>
+			<div className='test__flex'>Test Here</div>
+			<img src={Image} alt='' height={100} width={100} srcset='' />
+		</React.Fragment>
+	)
 }
